49d92c8a24eff5aa6db45efe1a4b670a187e4e4f
Anders Flaten <anders.flaten@nav.no>
2022-11-15 14:01:56 +0100

    TOB-2502: Bytter fra onprem-kafka til Aiven-kafka.
    Co-authored-by: Per Birger Eriksen <per.birger.eriksen@nav.no>

--- src/main/kotlin/no/nav/sokos/pdladapter/config/Configuration.kt
+++ src/main/kotlin/no/nav/sokos/pdladapter/config/Configuration.kt
@@ -1,29 +1,21 @@
 package no.nav.sokos.pdladapter.config
 
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG
-import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
+import java.util.*
 import mu.KotlinLogging
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_RECORDS_CONFIG
-import org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG
-import org.apache.kafka.common.config.SaslConfigs
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.apache.kafka.common.serialization.StringDeserializer
-import java.util.*
 
 private val logger = KotlinLogging.logger { }
 
-private val useAuthentication: Boolean = readProperty("USE_AUTHENTICATION", default = "true") != "false"
-
 class Configuration(
     val httpPort: Int = readProperty("HTTP_PORT", default = "8080").toInt(),
-    val kafkaConsumerConfig: KafkaConsumerConfig = KafkaConsumerConfig(),
+    val kafkaConfig: KafkaConfig = KafkaConfig(),
+    val kafkaConsumerConfig: KafkaConsumerConfig = KafkaConsumerConfig(kafkaConfig = kafkaConfig),
     val urMqProducerConfig: MqProducerConfig = MqProducerConfig(
         queue = readProperty("UR_MQ_PRODUCER_QUEUE"),
         host = readProperty("UR_MQ_HOST"),
@@ -44,35 +36,48 @@ class Configuration(
     )
 )
 
-class KafkaConsumerConfig(
-    val onPremBrokers: String = readProperty("ON_PREM_KAFKA_BROKERS"),
-    val topic: String = readProperty("KAFKA_CONSUMER_TOPIC"),
-    val schemaRegistryUrl: String = readProperty("KAFKA_SCHEMA_REGISTRY"),
-    private val groupId: String = readProperty("KAFKA_CONSUMER_GROUP_ID"),
-    private val username: String = readProperty("KAFKA_CONSUMER_USERNAME"),
-    private val password: String = readProperty("KAFKA_CONSUMER_PASSWORD"),
+data class KafkaConfig(
+    val brokers: String = readProperty("KAFKA_BROKERS"),
+    val schemaRegistry: String = readProperty("KAFKA_SCHEMA_REGISTRY"),
+    val schemaRegistryUser: String = readProperty("KAFKA_SCHEMA_REGISTRY_USER"),
+    val schemaRegistryPassword: String = readProperty("KAFKA_SCHEMA_REGISTRY_PASSWORD"),
+    val useSSLSecurity: Boolean = readProperty("KAFKA_USE_SSL_SECURITY", "true") == "true",
     val propMap: Properties = Properties().apply {
-        if (useAuthentication) {
-            put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
-            put(SaslConfigs.SASL_MECHANISM, "PLAIN")
-            put(
-                SaslConfigs.SASL_JAAS_CONFIG,
-                """org.apache.kafka.common.security.plain.PlainLoginModule required username="$username" password="$password";"""
-            )
+        if (useSSLSecurity) {
+            put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
+            put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
+            put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks")
+            put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
+            put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, readProperty("KAFKA_TRUSTSTORE_PATH"))
+            put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, readProperty("KAFKA_CREDSTORE_PASSWORD"))
+            put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, readProperty("KAFKA_KEYSTORE_PATH"))
+            put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, readProperty("KAFKA_CREDSTORE_PASSWORD"))
         }
-        put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
-        put(BOOTSTRAP_SERVERS_CONFIG, onPremBrokers)
-        put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
-        put(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
-        put(MAX_POLL_RECORDS_CONFIG, "1")
-        put(MAX_POLL_INTERVAL_MS_CONFIG, "200000")
-        put(ENABLE_AUTO_COMMIT_CONFIG, "false")
-        put(AUTO_OFFSET_RESET_CONFIG, "none") //TODO Implementere støtte for å håndtere at man mister offset for topic
-        put(SPECIFIC_AVRO_READER_CONFIG, true)
-        put(SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl)
-        put(BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
-    }
+        put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokers)
+        put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry)
+        put(SchemaRegistryClientConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO")
+        put(SchemaRegistryClientConfig.USER_INFO_CONFIG, "${schemaRegistryUser}:$schemaRegistryPassword")
+    },
 )
+
+data class KafkaConsumerConfig(
+    val topic: String = readProperty("KAFKA_CONSUMER_TOPIC"),
+    val consumerGroupId: String = readProperty("KAFKA_CONSUMER_GROUP_ID"),
+    val offsetReset: String = readProperty("KAFKA_CONSUMER_OFFSET_RESET"),
+    val kafkaConfig: KafkaConfig,
+    val propMap: Properties = Properties().apply {
+        put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroupId)
+        put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
+        put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
+        put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1")
+        put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "200000")
+        put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+        put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetReset)
+        put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed")
+        putAll(kafkaConfig.propMap)
+    },
+)
+
 class MqProducerConfig(
     val password: String,
     val username: String,
